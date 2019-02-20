a78b3085e00703efa9c98dd5ef61fe50ac1cab67
Raphael Peters <rappet@rappet.de>
2019-02-20 23:58:02 +0100

    :white_check_mark: Added tests for CreatePageMeta

--- pagemeta_test.go
+++ pagemeta_test.go
@@ -7,46 +7,78 @@ import (
 	"testing"
 )
 
-const exampleFile string = `
-<!DOCTYPE html>
-<html>
-<body>
-	<a href="http://example.org/foo">foo</a>
-	<img src="http://example.org/a.png" alt="bar"/>
-	<a href="http://example.org/baz">
-		<img src="http://example.org/b.png" alt="foobar"/>
-	</a>
-</body>
-</html>
-`
-
 func parse_url(s string) url.URL {
 	u, _ := url.Parse(s)
 	return *u
 }
 
-var exampleMeta = &PageMeta{
-	Title: "",
-	Links: []LinkMeta{
-		LinkMeta{parse_url("http://example.org/foo"), "foo"},
-		LinkMeta{parse_url("http://example.org/baz"), ""},
+var createMetaTests = []struct {
+	in  string
+	out *PageMeta
+}{
+	{
+		`
+			<!DOCTYPE html>
+			<html>
+			<body>
+				<a href="http://example.org/foo">foo</a>
+				<img src="http://example.org/a.png" alt="bar"/>
+			</body>
+			</html>
+		`,
+		&PageMeta{
+			Links: []LinkMeta{
+				LinkMeta{parse_url("http://example.org/foo"), "foo"},
+			},
+			Images: []ImageMeta{
+				ImageMeta{parse_url("http://example.org/a.png"), "bar", 0, 0},
+			},
+		},
 	},
-	Images: []ImageMeta{
-		ImageMeta{parse_url("http://example.org/a.png"), "bar", 0, 0},
-		ImageMeta{parse_url("http://example.org/b.png"), "foobar", 0, 0},
+	{
+		`
+			<body>
+				<img src="http://example.org/size.jpg" alt="size" width="123" height="456"/>
+			</body>
+		`,
+		&PageMeta{
+			Links: []LinkMeta{},
+			Images: []ImageMeta{
+				ImageMeta{parse_url("http://example.org/size.jpg"), "size", 123, 456},
+			},
+		},
+	},
+	{
+		`
+			<body>
+				<a href="http://example.org/baz">
+					<img src="http://example.org/b.png" alt="foobar"/>
+				</a>
+			</body>
+		`,
+		&PageMeta{
+			Links: []LinkMeta{
+				LinkMeta{parse_url("http://example.org/baz"), ""},
+			},
+			Images: []ImageMeta{
+				ImageMeta{parse_url("http://example.org/b.png"), "foobar", 0, 0},
+			},
+		},
 	},
 }
 
 func TestCreatePageMeta(t *testing.T) {
-	r := strings.NewReader(exampleFile)
-	meta, err := CreatePageMeta(r)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !reflect.DeepEqual(meta, exampleMeta) {
-		t.Logf("extracted: %v", meta)
-		t.Logf("expected: %v", exampleMeta)
-		t.Fatal("example meta and extracted meta are not equal")
+	for _, tt := range createMetaTests {
+		r := strings.NewReader(tt.in)
+		meta, err := CreatePageMeta(r)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !reflect.DeepEqual(meta, tt.out) {
+			t.Logf("extracted: %v", meta)
+			t.Logf("expected: %v", tt.out)
+			t.Fatal("example meta and extracted meta are not equal")
+		}
 	}
 }
 
